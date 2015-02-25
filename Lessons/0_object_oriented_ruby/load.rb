48d2817a3c706157d626a6d37cf5ce7514b42097
nbhartiya <neeharika@alum.mit.edu>
2015-02-25 14:41:02 -0800

    cleans up load.rb, renames toydog.rb

--- Lessons/0_object_oriented_ruby/load.rb
+++ Lessons/0_object_oriented_ruby/load.rb
@@ -1,6 +1,7 @@
 # This file simply loads the files in this project into your console
+$:.unshift(__dir__)
 
-require './dog_skills.rb'
-require './animal.rb'
-require './dog.rb'
-require './toydog.rb'
+require 'dog_skills'
+require 'animal'
+require 'dog'
+require 'toy_dog'
